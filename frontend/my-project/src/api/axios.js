17c353c44ab2671fa4b41e6b313cff32334f36be
sikindra97 <sikindramahto374@gmail.com>
2026-09-01 19:53:06 +0530

    Update axios.js

--- frontend/my-project/src/api/axios.js
+++ frontend/my-project/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"http://localhost:5000/api",
-
+  baseURL:
+    import.meta.env.VITE_BACKEND_URL ||
+    "http://localhost:5000/api",
   withCredentials: true,
 
   headers: {
